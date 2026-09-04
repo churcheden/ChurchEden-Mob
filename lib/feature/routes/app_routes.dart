265ac8a482337dc3c65c54c0c06befd5c77fae6d
Randynot <prempehagyemang62@gmail.com>
2026-09-04 23:44:12 +0000

    added the new stuff

--- lib/feature/routes/app_routes.dart
+++ lib/feature/routes/app_routes.dart
@@ -1,12 +1,15 @@
+import 'package:church_eden_mob/feature/auth/presentation/screens/login_screen.dart';
 import 'package:church_eden_mob/feature/routes/routes_const.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRoutes {
   late final GoRouter routes = GoRouter(
     initialLocation: RoutesConstant.loginPage,
-      routes: [
-
-      ]);
-
-
+    routes: [
+      GoRoute(
+        path: RoutesConstant.loginPage,
+        builder: (context, state) => LoginScreen(),
+      ),
+    ],
+  );
 }
