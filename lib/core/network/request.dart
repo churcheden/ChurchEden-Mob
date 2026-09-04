aae5056008716a3f3adaa04a3c9995adda79f73d
Randynot <prempehagyemang62@gmail.com>
2026-09-04 00:27:58 +0000

    added the request

--- lib/core/network/request.dart
+++ lib/core/network/request.dart
@@ -1,3 +1,14 @@
-class Request<T>{
-  
+abstract class Request{
+  final String baseUrl;
+  final String endPoint;
+  final HttpMethod method;
+  final dynamic Function(dynamic)decoder;
+
+ Request({
+
+required this.baseUrl, required this.endPoint, required this.method, required this.decoder});
+
+
 }
+
+enum HttpMethod { get, post, put, delete }
