44e60456d622b5d24f6836024365d5e8d712a331
Randynot <prempehagyemang62@gmail.com>
2026-09-05 04:42:29 +0000

    added the new church tile

--- lib/feature/auth/presentation/screens/find_your_church_page.dart
+++ lib/feature/auth/presentation/screens/find_your_church_page.dart
@@ -2,6 +2,7 @@ import 'package:church_eden_mob/feature/auth/presentation/widget/nearby_church_t
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FindYourChurchPage extends HookWidget {
   const FindYourChurchPage({super.key});
@@ -12,64 +13,127 @@ class FindYourChurchPage extends HookWidget {
       backgroundColor: const Color(0xFFF5F0E8),
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: .center,
           children: [
             CircleAvatar(
-              backgroundColor: Colors.white,
-              child: SvgPicture.asset("assets/Images/church-stroke.svg"),
-            ),
-            Text('Find your church'),
-            Text(
-              'Search for your church below to get started or request to join',
-            ),
+              radius: 42,
 
-            SearchBar(
-              leading: SvgPicture.asset('assets/Images/search.svg'),
-              trailing: [
-                SvgPicture.asset('assets/Images/horizontal-stroke.svg'),
-              ],
-              hintText: 'Search by Church name',
-              elevation: WidgetStatePropertyAll(0),
-              backgroundColor: WidgetStatePropertyAll(Colors.white),
-              shape: WidgetStatePropertyAll(
-                RoundedRectangleBorder(borderRadius: .circular(20)),
+              backgroundColor: Color(0xFFB8860B),
+
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: const Color(0xFFF5F0E8),
+                child: SizedBox(
+                  height: 40,
+                  width: 40,
+                  child: SvgPicture.asset(
+                    "assets/Images/church-stroke.svg",
+                    colorFilter: const ColorFilter.mode(Color(0xFFB8860B), BlendMode.srcIn),
+                  ),
+                ),
               ),
             ),
-            Row(
-              mainAxisAlignment: .spaceBetween,
-              children: [
-                Text('Nearby Churches'),
-                Row(
-                  children: [
-                    SvgPicture.asset('assets/Images/location.svg'),
-                    Text('Accra,Ghana'),
-                  ],
-                ),
-              ],
+            SizedBox(height: 20),
+            Text(
+              'Find your church',
+              style: GoogleFonts.playfair(fontWeight: .bold, fontSize: 44),
             ),
+            Text(
+              "Search for your church below to get started ",
+              style: GoogleFonts.inter(fontSize: 15),
+            ),
+            Text("or request to join.", style: GoogleFonts.inter(fontSize: 15)),
+            SizedBox(height: 22),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: SearchBar(
+                padding: WidgetStatePropertyAll(.symmetric(horizontal: 22)),
+                leading: SvgPicture.asset('assets/Images/search.svg'),
+                trailing: [
+                  SvgPicture.asset('assets/Images/horizontal-stroke.svg'),
+                ],
+                hintText: 'Search by Church name',
+                hintStyle: WidgetStatePropertyAll(
+                  GoogleFonts.inter(color: Colors.grey),
+                ),
+                elevation: WidgetStatePropertyAll(0),
+                backgroundColor: WidgetStatePropertyAll(Colors.white),
+                shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(borderRadius: .circular(20)),
+                ),
+              ),
+            ),
+            SizedBox(height: 22),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Row(
+                mainAxisAlignment: .spaceBetween,
+                children: [
+                  Text(
+                    'Available Churches',
+                    style: GoogleFonts.inter(fontWeight: .bold, fontSize: 20),
+                  ),
+                  Row(
+                    spacing: 10,
+                    children: [
+                      SizedBox(
+                        height: 15,
+                        child: SvgPicture.asset('assets/Images/location.svg'),
+                      ),
+                      Text(
+                        'Accra,Ghana',
+                        style: GoogleFonts.inter(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 22),
             NearbyChurchTile(
               title: 'Revival Chapel',
               subtitle: 'Accra,Gh',
-              trailing: Icon(Icons.arrow_forward_ios_rounded),
+              trailing: Icon(
+                Icons.arrow_forward_ios_rounded,
+                color: Color(0xFFB8860B),
+                size: 18,
+              ),
               leading: 'assets/Images/church-stroke.svg',
+              isSecond: false,
             ),
-            SizedBox(height: 22,),
+            SizedBox(height: 22),
             NearbyChurchTile(
               title: "Can't find your Church ?",
               subtitle: 'Request to add your Church to ChurchEden',
               trailing: TextButton(
+                style: TextButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: .circular(10),
+                    side: const BorderSide(color: Color(0xFFB8860B)),
+                  ),
+                ),
                 onPressed: () {},
-                child: Text('Request Church'),
+                child: Text(
+                  'Request Church',
+                  style: GoogleFonts.inter(
+                    color: Color(0xFFB8860B),
+                    fontWeight: .bold,
+                  ),
+                ),
               ),
               leading: 'assets/Images/church-stroke.svg',
+              isSecond: true,
             ),
+            SizedBox(height: 22),
             Row(
-              spacing:10,
+              spacing: 10,
               mainAxisAlignment: .center,
               children: [
                 SvgPicture.asset('assets/Images/lock.svg'),
                 Text('Secure and private'),
               ],
             ),
+            SizedBox(height: 90),
           ],
         ),
       ),
--- lib/feature/auth/presentation/widget/nearby_church_tile.dart
+++ lib/feature/auth/presentation/widget/nearby_church_tile.dart
@@ -5,29 +5,78 @@ import 'package:google_fonts/google_fonts.dart';
 class NearbyChurchTile extends StatelessWidget {
   final String title, subtitle;
   final Widget trailing;
-final String leading;
+  final String leading;
+  final bool isSecond;
+
   const NearbyChurchTile({
     super.key,
     required this.title,
     required this.subtitle,
-    required this.trailing, required this.leading,
+    required this.trailing,
+    required this.leading,
+    required this.isSecond,
   });
 
   @override
   Widget build(BuildContext context) {
-
-    final height = MediaQuery.of(context).size.height;
+    final height = MediaQuery
+        .of(context)
+        .size
+        .height;
     return SizedBox(
-      height: height * 0.09,
-      child: ListTile(
-        leading: SvgPicture.asset(leading),
-        tileColor: Colors.white,
-        trailing: trailing,
-        title: Text(title),
-        subtitle: Text(subtitle),
-        titleTextStyle: GoogleFonts.inter(color: Colors.black,fontWeight: .bold,),
-        subtitleTextStyle: GoogleFonts.inter(color: Colors.black),
-        shape: RoundedRectangleBorder(borderRadius: .circular(22)),
+      height: height * 0.13,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: ListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+          leading: !isSecond
+              ? Container(
+            height: 54,
+            width: 54,
+            decoration: BoxDecoration(
+              shape: .rectangle,
+              color: const Color(0xFF1B2A41),
+              borderRadius: .circular(12),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset(leading,
+                  colorFilter: const ColorFilter.mode(
+                      Colors.white, BlendMode.srcIn), ),
+            ),
+          )
+              : CircleAvatar(
+            radius: 22,
+
+            backgroundColor: Color(0xFFB8860B),
+            child: CircleAvatar(
+              radius: 20,
+              backgroundColor: const Color(0xFFF5F0E8),
+              child: SvgPicture.asset(leading,
+                colorFilter: const ColorFilter.mode(
+                    Color(0xFFB8860B), BlendMode.srcIn),),
+            ),
+          ),
+          tileColor: !isSecond ? Colors.white : const Color(0xFFF5F0E8),
+          trailing: trailing,
+          title: Text(title),
+          subtitle: Text(subtitle),
+          titleTextStyle: GoogleFonts.inter(
+            color: Colors.black,
+            fontWeight: .bold,
+            fontSize: 22,
+          ),
+          subtitleTextStyle: GoogleFonts.inter(
+            color: Colors.black,
+            fontSize: 12,
+          ),
+          shape: !isSecond
+              ? RoundedRectangleBorder(borderRadius: .circular(22))
+              : RoundedRectangleBorder(
+            borderRadius: .circular(22),
+            side: BorderSide(color: Color(0xFFB8860B)),
+          ),
+        ),
       ),
     );
   }
