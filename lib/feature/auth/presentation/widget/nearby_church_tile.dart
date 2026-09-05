e36adefb35921e3f2cdcff29d8848f50f2561540
Randynot <prempehagyemang62@gmail.com>
2026-09-05 04:54:05 +0000

    added the new church request

--- lib/feature/auth/presentation/widget/nearby_church_tile.dart
+++ lib/feature/auth/presentation/widget/nearby_church_tile.dart
@@ -19,10 +19,7 @@ class NearbyChurchTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery
-        .of(context)
-        .size
-        .height;
+    final height = MediaQuery.of(context).size.height;
     return SizedBox(
       height: height * 0.13,
       child: Padding(
@@ -31,32 +28,44 @@ class NearbyChurchTile extends StatelessWidget {
           contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 12),
           leading: !isSecond
               ? Container(
-            height: 54,
-            width: 54,
-            decoration: BoxDecoration(
-              shape: .rectangle,
-              color: const Color(0xFF1B2A41),
-              borderRadius: .circular(12),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SvgPicture.asset(leading,
-                  colorFilter: const ColorFilter.mode(
-                      Colors.white, BlendMode.srcIn), ),
-            ),
-          )
+                  height: 54,
+                  width: 54,
+                  decoration: BoxDecoration(
+                    shape: .rectangle,
+                    color: const Color(0xFF1B2A41),
+                    borderRadius: .circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset(
+                      leading,
+                      colorFilter: const ColorFilter.mode(
+                        Colors.white,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
+                )
               : CircleAvatar(
-            radius: 22,
+                  radius: 22,
 
-            backgroundColor: Color(0xFFB8860B),
-            child: CircleAvatar(
-              radius: 20,
-              backgroundColor: const Color(0xFFF5F0E8),
-              child: SvgPicture.asset(leading,
-                colorFilter: const ColorFilter.mode(
-                    Color(0xFFB8860B), BlendMode.srcIn),),
-            ),
-          ),
+                  backgroundColor: Color(0xFFB8860B),
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: const Color(0xFFF5F0E8),
+                    child: Stack(
+                      children: [
+                        SvgPicture.asset(
+                          leading,
+                          colorFilter: const ColorFilter.mode(
+                            Color(0xFFB8860B),
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
           tileColor: !isSecond ? Colors.white : const Color(0xFFF5F0E8),
           trailing: trailing,
           title: Text(title),
@@ -73,9 +82,9 @@ class NearbyChurchTile extends StatelessWidget {
           shape: !isSecond
               ? RoundedRectangleBorder(borderRadius: .circular(22))
               : RoundedRectangleBorder(
-            borderRadius: .circular(22),
-            side: BorderSide(color: Color(0xFFB8860B)),
-          ),
+                  borderRadius: .circular(22),
+                  side: BorderSide(color: Color(0xFFB8860B)),
+                ),
         ),
       ),
     );
